2acbe92f099f636ea8c402b03f9e2fade72b812b
Cryptodroid <tiiimleee@gmail.com>
2017-03-15 00:09:44 +0700

    Update coinmarketgraph.py

--- coinmarketgraph.py
+++ coinmarketgraph.py
@@ -1,36 +1,39 @@
 import requests
 import matplotlib.pyplot as plt
 
+# Get datas from coinmarketcap api
 def query():
-    url = 'https://api.coinmarketcap.com/v1/ticker'
-    req = requests.get(url)
+    req = requests.get('https://api.coinmarketcap.com/v1/ticker')
     response = req.json()
     return response
 
-alldat = query()
-absc = []
-ordn = []
+# select coins with a daily volume above 10K $
+alldata = query()
+marketcap = []
+volume = []
 labels = []
 data = []
-for i in alldat:
+for i in alldata:
     if i['24h_volume_usd'] != None:
         i['24h_volume_usd'] = float(i['24h_volume_usd'])
         if i['market_cap_usd'] != None:
             i['market_cap_usd'] = float(i['market_cap_usd'])
             if i['24h_volume_usd'] > 10000:
-                absc.append(i['market_cap_usd'])
-                ordn.append(i['24h_volume_usd'])
+                marketcap.append(i['market_cap_usd'])
+                volume.append(i['24h_volume_usd'])
                 labels.append(i['name'])
 
+# Display the datas with matplotlib
 fig = plt.figure()
 ax = plt.gca()
-ax.scatter(absc, ordn, label = 'name')
+ax.scatter(marketcap, volume, label = 'name')
 ax.set_xscale('log')
 ax.set_yscale('log')
 
+# add legend for each coins
 z = 0
 for i in labels:
-    plt.annotate(i, xy=(absc[z], ordn[z]), xycoords='data',
+    plt.annotate(i, xy=(marketcap[z], volume[z]), xycoords='data',
     xytext=(-20,20), textcoords='offset points',ha = 'right', va = 'bottom',
     bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
     arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
